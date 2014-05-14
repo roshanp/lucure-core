bbdfd70c29041d047994e0d47b1afab68b3232cf
Punnoose, Roshan <rashan.punnrose@merck.com>
2014-05-14 00:15:42 -0400

    Getting functional test to run

--- src/test/java/org/lucure/core/LucureFunctionalTest.java
+++ src/test/java/org/lucure/core/LucureFunctionalTest.java
@@ -35,7 +35,7 @@ import org.junit.runners.JUnit4;
  *
  */
 @RunWith(JUnit4.class)
-public class LucureFunctionalTests {
+public class LucureFunctionalTest {
 
   public static final Version LUCENE_VERSION = Version.LUCENE_47;
 

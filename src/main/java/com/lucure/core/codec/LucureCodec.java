1d3968f5f06c287f96d6e065f820fe3a4404920e
Roshan Punnoose <roshanp@gmail.com>
2014-10-20 14:58:09 -0400

    Removing DocValues use from LucureCodec

--- src/main/java/com/lucure/core/codec/LucureCodec.java
+++ src/main/java/com/lucure/core/codec/LucureCodec.java
@@ -19,12 +19,16 @@ package com.lucure.core.codec;
 
 import org.apache.lucene.codecs.*;
 import org.apache.lucene.codecs.lucene40.Lucene40LiveDocsFormat;
-import org.apache.lucene.codecs.lucene42.Lucene42NormsFormat;
 import org.apache.lucene.codecs.lucene42.Lucene42TermVectorsFormat;
 import org.apache.lucene.codecs.lucene46.Lucene46FieldInfosFormat;
 import org.apache.lucene.codecs.lucene46.Lucene46SegmentInfoFormat;
 import org.apache.lucene.codecs.lucene49.Lucene49NormsFormat;
 import org.apache.lucene.codecs.perfield.PerFieldPostingsFormat;
+import org.apache.lucene.index.*;
+import org.apache.lucene.util.Bits;
+import org.apache.lucene.util.BytesRef;
+
+import java.io.IOException;
 
 /**
  * Implements the Lucene 4.6 index format, with configurable per-field postings
@@ -40,7 +44,8 @@ import org.apache.lucene.codecs.perfield.PerFieldPostingsFormat;
 // if they are backwards compatible or smallish we can probably do the backwards in the postingsreader
 // (it writes a minor version, etc).
 public class LucureCodec extends Codec {
-  private final StoredFieldsFormat fieldsFormat = new LucureStoredFieldsFormat();
+    public static final String LUCURE_CODEC_NAME = "Lucure";
+    private final StoredFieldsFormat fieldsFormat = new LucureStoredFieldsFormat();
   private final TermVectorsFormat vectorsFormat = new Lucene42TermVectorsFormat();
   private final FieldInfosFormat fieldInfosFormat = new Lucene46FieldInfosFormat();
   private final SegmentInfoFormat segmentInfosFormat = new Lucene46SegmentInfoFormat();
@@ -55,7 +60,7 @@ public class LucureCodec extends Codec {
 
   /** Sole constructor. */
   public LucureCodec() {
-    super("Lucure");
+    super(LUCURE_CODEC_NAME);
   }
   
   @Override
@@ -99,10 +104,116 @@ public class LucureCodec extends Codec {
   
   @Override
   public final DocValuesFormat docValuesFormat() {
-    throw new UnsupportedOperationException("DocValues is not supported in Lucure");
+    return new DocValuesFormat(LUCURE_CODEC_NAME) {
+
+        @Override
+        public DocValuesConsumer fieldsConsumer(
+          SegmentWriteState state) throws IOException {
+            return new DocValuesConsumer() {
+
+                @Override
+                public void addNumericField(
+                  FieldInfo field, Iterable<Number> values) throws IOException {
+
+                }
+
+                @Override
+                public void addBinaryField(
+                  FieldInfo field, Iterable<BytesRef> values)
+                  throws IOException {
+
+                }
+
+                @Override
+                public void addSortedField(
+                  FieldInfo field, Iterable<BytesRef> values,
+                  Iterable<Number> docToOrd) throws IOException {
+
+                }
+
+                @Override
+                public void addSortedNumericField(
+                  FieldInfo field, Iterable<Number> docToValueCount,
+                  Iterable<Number> values) throws IOException {
+
+                }
+
+                @Override
+                public void addSortedSetField(
+                  FieldInfo field, Iterable<BytesRef> values,
+                  Iterable<Number> docToOrdCount, Iterable<Number> ords)
+                  throws IOException {
+
+                }
+
+                @Override
+                public void close() throws IOException {
+
+                }
+            };
+        }
+
+        @Override
+        public DocValuesProducer fieldsProducer(
+          SegmentReadState state) throws IOException {
+            return new DocValuesProducer() {
+                @Override
+                public NumericDocValues getNumeric(
+                  FieldInfo field) throws IOException {
+                    return null;
+                }
+
+                @Override
+                public BinaryDocValues getBinary(
+                  FieldInfo field) throws IOException {
+                    return null;
+                }
+
+                @Override
+                public SortedDocValues getSorted(
+                  FieldInfo field) throws IOException {
+                    return null;
+                }
+
+                @Override
+                public SortedNumericDocValues getSortedNumeric(
+                  FieldInfo field) throws IOException {
+                    return null;
+                }
+
+                @Override
+                public SortedSetDocValues getSortedSet(
+                  FieldInfo field) throws IOException {
+                    return null;
+                }
+
+                @Override
+                public Bits getDocsWithField(
+                  FieldInfo field) throws IOException {
+                    return null;
+                }
+
+                @Override
+                public void checkIntegrity() throws IOException {
+
+                }
+
+                @Override
+                public long ramBytesUsed() {
+                    return 0;
+                }
+
+                @Override
+                public void close() throws IOException {
+
+                }
+            };
+        }
+    };
   }
 
-  private final PostingsFormat defaultFormat = PostingsFormat.forName("Lucure");
+  private final PostingsFormat defaultFormat = PostingsFormat.forName(
+    LUCURE_CODEC_NAME);
 
   private final NormsFormat normsFormat = new Lucene49NormsFormat();
 
